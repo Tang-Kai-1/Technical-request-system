6b9cc252dfa40e8d68325dd216f73852345fd166
Toms Prūsis <atlas190028@gmail.com>
2022-12-07 03:26:08 +0200

    minor fixes

--- src/test/java/com/example/techrequests/TechRequestsApplicationTests.java
+++ src/test/java/com/example/techrequests/TechRequestsApplicationTests.java
@@ -20,11 +20,12 @@ class TechRequestsApplicationTests {
     @Autowired
     RequestsService service;
 
+    private final CreateRequest createRequest = new CreateRequest("Dators", "Darba", "vajag darbam", "izskata");
     @Test
     @Transactional
     @DisplayName("Spēj pievienot pieprasījumu")
     void addRequest(){
-        Request request = service.createRequest(new CreateRequest("Dators", "Darba", "vajag darbam", "izskata")).getBody();
+        Request request = service.createRequest(createRequest).getBody();
         assertTrue(request!=null);
     }
 
@@ -32,7 +33,7 @@ class TechRequestsApplicationTests {
     @Transactional
     @DisplayName("Spēj dzēst pieprasījumu")
     void removeRequest(){
-        Request request = service.createRequest(new CreateRequest("Dators", "Darba", "vajag darbam", "izskata")).getBody();
+        Request request = service.createRequest(createRequest).getBody();
         Request requestDeleted = service.deleteRequest(request.getId()).getBody();
         assertEquals(request,requestDeleted);
     }
@@ -41,7 +42,7 @@ class TechRequestsApplicationTests {
     @Transactional
     @DisplayName("Spēj mainīt pieprasījuma statusu")
     void updateRequest(){
-        Request request = service.createRequest(new CreateRequest("Dators", "Darba", "vajag darbam", "izskata")).getBody();
+        Request request = service.createRequest(createRequest).getBody();
         String originalStatus = request.getStatus();
         Request requestUpdated = service.changeRequestStatus(request.getId(),"noraidīts").getBody();
         assertTrue(originalStatus!=requestUpdated.getStatus());
@@ -52,7 +53,7 @@ class TechRequestsApplicationTests {
     @DisplayName("Spēj izgūt visus pieprasījumus")
     void getAllRequests(){
         List<Request> requests = new ArrayList<>();
-        requests.add(service.createRequest(new CreateRequest("Dators", "Darba", "vajag darbam", "izskata")).getBody());
+        requests.add(service.createRequest(createRequest).getBody());
         requests.add(service.createRequest(new CreateRequest("Pele", "ergonomiskā", "vajag darbam", "izskata")).getBody());
         requests.add(service.createRequest(new CreateRequest("Klaviatūra", "mehāniskā", "vajag darbam", "izskata")).getBody());
         assertEquals(requests,service.getRequests().getBody());
